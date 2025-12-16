25a676a253098b70e4e833963eb0a24bfc147adc
Maxime Chopin <mchopin@f0r3s2.codam.nl>
2025-12-16 21:41:16 +0100

    initial commit

---
+++ seeya.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+git add .
+git commit -m "small update"
+git push
+git push github master
+sleep 2 
+clear
+tree
+git status
+sleep 3.5
+clear
+cowsay -f eyes "ALL DONE !!! You fat bastard, get outta here ;) "
+sleep 5
+clear
+cowsay -f eyes "Come on go away..."
+sleep 3
+clear
+cowsay -f dragon-and-cow "getcho bingjao ass tf outta here"
+
