064184ff53bf450dead7750eb77cb326ef705883
Maxime Chopin <mchopin@f0r5s1.codam.nl>
2025-12-28 17:33:41 +0100

    small update

--- input_parcer.c
+++ input_parcer.c
@@ -6,27 +6,50 @@
 /*   By: mchopin <mchopin@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/12/17 17:17:52 by mchopin       #+#    #+#                 */
-/*   Updated: 2025/12/22 21:52:52 by mchopin       ########   odam.nl         */
+/*   Updated: 2025/12/28 17:24:48 by mchopin       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int	ft_is_valid(char *s)
+{
+	int	i;
+
+	i = 0;
+	if (s[i] == '+' || s[i] == '-')
+		i++;
+	if (s[i] == '\0')
+		return (0);
+	while (s[i])
+	{
+		if (!ft_isdigit(s[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
 char	**manage_input(int argc, char **argv)
 {
 	char	**args;
+	int		i;
 
 	if (argc == 2)
 	{
-		// import libft for split
-		// also makefile
 		args = ft_split(argv[1], ' ');
 		if (!args || !args[0])
 		{
-			// if (args)
-			// free(args);
-			// shouldn't have to free here I think
 			return (NULL);
+		}
+		i = 0;
+		while (argv[i] != NULL)
+		{
+			if (!ft_is_valid(args[i]))
+			{
+				ft_printf("%s", "ERROR, invalid input");
+				return (free(args), NULL);
+			}
+			i++;
 		}
 		// gotta free args here
 		return (args);
@@ -37,18 +60,19 @@ char	**manage_input(int argc, char **argv)
 
 int	main(int argc, char **argv)
 {
-  int i;
-  i = 0;
-	char **args;
+	int		i;
+	char	**args;
+
+	i = 0;
 	if (argc < 2)
 		return (0);
 	args = manage_input(argc, argv);
 	if (!args)
 		return (1);
-  while(args[i])
-  {
-    ft_printf("args[%d] = %s\n",i, args[i]);
-    i++;
-  }
+	while (args[i])
+	{
+		ft_printf("args[%d] = %s\n", i, args[i]);
+		i++;
+	}
 	return (0);
 }
