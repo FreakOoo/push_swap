5ab0ef9be722e30397769058a217d18bad6ae666
Maxime Chopin <mchopin@f1r2s16.codam.nl>
2026-02-03 21:00:36 +0100

    small update

--- target_node.c
+++ target_node.c
@@ -6,11 +6,27 @@
 /*   By: mchopin <mchopin@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/01/30 23:27:54 by mchopin       #+#    #+#                 */
-/*   Updated: 2026/02/01 21:38:57 by mchopin       ########   odam.nl         */
+/*   Updated: 2026/02/03 20:59:41 by mchopin       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	set_index(t_node *a)
+{
+	int	i;
+	int	median;
+
+	i = 0;
+	median = stack_len(a) / 2;
+	while (a)
+	{
+		a->index = i;
+		a->above_median = (i <= median);
+		a = a->next;
+		i++;
+	}
+}
 
 t_node	*find_smallest(t_node *a)
 {
@@ -54,30 +70,22 @@ void	smallest_bigger(t_node **a, t_node **b)
 		my_beloved = find_smallest(*a);
 	while (*a != my_beloved)
 		ra(a);
-			//this still needs to adapt wether its cheaper to rotate or rev rotate
+	// this still needs to adapt wether its cheaper to rotate or rev rotate
 	pa(a, b);
-	
+}
+
+void	have_crush(t_node *a, t_node *b)
+{
+	while (b)
+	{
+		b->target = find_smallest_bigger(a, b->value);
+		if (!b->target)
+			b->target = find_smallest(a);
+		b = b->next;
+	}
 }
 // also need to compare to median, calculate median
 // and index
 // need to figure out wether to use ra or rra
-//now I need to calculate the cost of bringing the target to the top, 
-//and if its cheaper with r,rra , rrr , rr instead of doing just ra.
-
-void	set_index(t_node *a)
-{
-	int	i;
-	int	median;
-
-	i = 0;
-	median = stack_len(a) / 2;
-	while (a)
-	{
-		a->index = i;
-		a->above_median = (i < median);
-		a = a->next;
-		i++;
-	}
-}
-
-// this is where I left off, still need to test.
+// now I need to calculate the cost of bringing the target to the top,
+// and if its cheaper with r,rra , rrr , rr instead of doing just ra.
