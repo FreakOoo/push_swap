97db109df6ab93b65568469872a48e6a24401618
Maxime Chopin <mchopin@f0r4s4.codam.nl>
2026-02-03 22:57:14 +0100

    small update

--- cost.c
+++ cost.c
@@ -6,9 +6,13 @@
 /*   By: mchopin <mchopin@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/02/03 20:36:12 by mchopin       #+#    #+#                 */
-/*   Updated: 2026/02/03 20:36:24 by mchopin       ########   odam.nl         */
+/*   Updated: 2026/02/03 22:57:11 by mchopin       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void calculate_cost(t_node **a, t_node **b)
+{
+    //calculate the cost here chud of the future.   
+}
